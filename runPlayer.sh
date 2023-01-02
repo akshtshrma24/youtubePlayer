18efbf223e5a1fef9851771fe609551e498f0de0
akshtshrma24 <akshitsharmaa65@gmail.com>
2023-01-02 11:35:36 -0800

    Rename test install pip3 in bash script

--- runPlayer.sh
+++ runPlayer.sh
@@ -3,18 +3,18 @@
 export GREEN='\033[0;32m'
 export NC='\033[0m'
 
-echo "${GREEN}Upgrading pip${NC}"
+echo "${GREEN}Installing pip${NC}"
+sudo apt install python3-pip
 
+echo "${GREEN}Upgrading pip${NC}"
 pip3 install --upgrade pip
 
 echo "${GREEN}Installing requirements${NC}"
-
 pip3 install -r requirements.txt
 
 export FLASK_APP=app.py
 
 echo "${GREEN}Making videos directory${NC}"
-
 mkdir videos
 
 flask run --host=0.0.0.0
