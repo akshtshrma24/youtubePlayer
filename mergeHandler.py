7ccf6e4fb86d39d08ce6b9a744ea297f66ee1147
akshtshrma24 <akshitsharmaa65@gmail.com>
2023-01-02 11:28:57 -0800

    Use Bash Script instead of Dockerfile, easier networking

--- mergeHandler.py
+++ mergeHandler.py
@@ -2,17 +2,14 @@ import pytube
 
 import tubePegHandler
 from directoryHandler import * 
-from logger import * 
 
 def download_merge(link):
     if("playlist" not in link):
-        warning("Not a playlist double check {}".format(link))
         if(not is_in_videos(link)):
             tph = tubePegHandler.tubePegHandler()
             audio = tph.download_audio_convert(link)
             tph.download_video_merge(link)
     else:
-        warning("Playlist double check {}".format(link))
         p = pytube.Playlist(link)
         for url in p.video_urls:
             name = p.title
---
+++ runPlayer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export GREEN='\033[0;32m'
+export NC='\033[0m'
+
+echo "${GREEN}Upgrading pip${NC}"
+
+pip3 install --upgrade pip
+
+echo "${GREEN}Installing requirements${NC}"
+
+pip3 install -r requirements.txt
+
+export FLASK_APP=app.py
+
+echo "${GREEN}Making videos directory${NC}"
+
+mkdir videos
+
+flask run --host=0.0.0.0
