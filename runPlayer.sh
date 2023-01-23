300b770deb84109e055d62a6a8d1025ba8882b42
akshtshrma24 <akshitsharmaa65@gmail.com>
2023-01-23 14:01:29 -0800

    Colored output in ./runPlayer

--- runPlayer.sh
+++ runPlayer.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 export GREEN='\033[0;32m'
+export CYAN='\033[0;36m'
 export NC='\033[0m'
 
-echo "${GREEN}Updating and upgrading apt-get${NC}"
+printf "${GREEN}Updating and upgrading apt-get${NC}\n"
 sudo apt-get update ; sudo apt-get upgrade
 
-echo "${GREEN}Installing ffmpeg${NC}"
+printf "${GREEN}Installing ffmpeg${NC}\n"
 sudo apt install --assume-yes ffmpeg 
 
-echo "${GREEN}Installing vlc${NC}"
+printf "${GREEN}Installing vlc${NC}\n"
 sudo apt install -y vlc 
 
-echo "${GREEN}Installing pip${NC}"
+printf "${GREEN}Installing pip${NC}\n"
 sudo apt install --assume-yes python3-pip 
 
-echo "${GREEN}Installing requirements${NC}"
+printf "${GREEN}Installing requirements${NC}\n"
 pip3 install pytube 
 
-echo "${GREEN}Making videos directory${NC}"
+printf "${GREEN}Making videos directory${NC}\n"
+mkdir ./videoDownloading/videos
 
+printf  "${GREEN}Starting Server${NC}\n"
+printf "${CYAN}running on $(ip addr | grep 192 | awk '{print $2}') ${NC}\n"
 python3 server.py
