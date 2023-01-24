6cb451e1e161d79bdd605c227a81b3aeaa3d6ebe
akshtshrma24 <akshitsharmaa65@gmail.com>
2023-01-23 16:57:02 -0800

    Add try except in get_title

--- videoDownloading/downloadFormating/downloader.py
+++ videoDownloading/downloadFormating/downloader.py
@@ -1,18 +1,25 @@
 import os
 
-from pytube import YouTube
+from pytube import YouTube, exceptions
 
 
 class downloader:
 
-    # Gets the title of the youtube video
+    # Gets the title of the youtube video, sometimes it fails retries until it gets it
 
     def get_title(self, link):
-        return YouTube(link).title
+        while True:
+            try:
+               title = YouTube(link).title
+            except exceptions.PytubeError:
+                continue
+            break
+        return title
 
     # Downloads the audio returns audio.mp4
 
     def download_audio(self, link):
+        
         YouTube(link).streams.filter(
             only_audio=True).desc().first().download(filename="audio.webm")
         return "audio.webm"
